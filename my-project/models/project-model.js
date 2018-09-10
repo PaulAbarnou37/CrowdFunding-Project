06bf31518b475a820a50e397ec70008df79a2909
Paul Abarnou <p.abarnou37@gmail.com>
2018-09-10 15:15:02 +0200

    added models

---
+++ my-project/models/project-model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+const projectSchema = new Schema({
+  // document structure & rules
+  name: { type: String, required: true },
+  shortDescription: { type: String, required: true },
+  longDescription: { type: String, required: true },
+  moneyExpected: { type: Number, required: true },
+  creationDate: { type: String, required: true },
+  endDate: { type: String, required: true },
+  comments: { type: [String]},
+  contributors: { type: [String]},
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User", 
+    required: true,
+  },
+}, {
+  timestamps: true
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+
+module.exports = Project;
