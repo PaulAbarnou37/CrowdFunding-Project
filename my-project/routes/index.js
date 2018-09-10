99541678bd68278f26176146e90ef978a614572f
John Dimitroff <johnmdimitroff@gmail.com>
2018-09-10 16:09:08 +0200

    new routes

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -3,12 +3,18 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+    //req.user comes from Passport's deserializeUser().
+  //it's the document of the currently logged in user
   res.render('index');
-});
 
+if (req.user) {
+  console.log("LOGGED IN!");
+} 
+else {
+  console.log ("LOGGED OUT");
+}
+
+res.render("index.hbs");
+});
 module.exports = router;
 
-
-YO JOHN
-
-YO PAUL
