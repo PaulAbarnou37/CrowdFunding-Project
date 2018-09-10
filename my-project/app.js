dd4161ca122edc61cb47f5065f928c82e126c8dc
Paul Abarnou <p.abarnou37@gmail.com>
2018-09-10 18:08:39 +0200

    test

--- my-project/app.js
+++ my-project/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/my-project', {useNewUrlParser: true})
+  .connect('mongodb://localhost/crowdfunding-project', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("views/index.hbs");
+  res.render("index.hbs");
 });
 //GET projects list page
 router.get("/projects-list", (req, res, next) => {
