38a36add92e900039897deb67fbcd8556897d8bb
Paul Abarnou <p.abarnou37@gmail.com>
2018-09-11 16:18:36 +0200

    single project view

--- my-project/routes/auth-router.js
+++ my-project/routes/auth-router.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-
 const bcrypt = require("bcrypt");
-
 const User = require("../models/user-model.js");
-
+const Project = require("../models/project-model.js");
 
 
 //GET signup page
@@ -21,10 +19,16 @@ router.post("/process-signup", (req, res, next) => {
 
   User.create({firstName, lastName, email, encryptedPassword})
   .then(UserDoc => {
-    req.flash("success", "Sign up success!");
-    res.redirect("/")
+    req.logIn(UserDoc, () => {
+    req.flash("success", "Sign Up success!");
+    res.redirect("/dashboard");
+  });
   })
   .catch(err => next(err));
+});
+
+router.get("/dashboard", (req, res, next) => {
+  res.render("dashboard.hbs")
 });
 
 //GET login page
@@ -49,7 +53,7 @@ User.findOne({email: {$eq:email}})
   }
   req.logIn(userDoc, () => {
     req.flash("success", "Log in success!");
-    res.redirect("/");
+    res.redirect("/dashboard");
   });
 })
   .catch(err => next(err));
@@ -62,6 +66,25 @@ router.get("/logout", (req, res, next) => {
   // save a flash message to display in the HOME page
   req.flash("success", "Logged out successfully!");
   res.redirect("/");
+});
+
+
+router.get("/projects-list/:projectid", (request,response,next)=>{
+  const { projectid } = request.params; // params pour recup ce quil ya dans l'url
+  Project.findOne({_id : { $eq: projectid } })
+      .then(data => {
+        console.log('MY DATAAAAAAAAA');
+        console.log(data);
+        // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        response.locals.projectData = data;
+        // to show result on browser and not the terminal: 
+        // response.send(data.body.artists.items);
+        response.render("project-page.hbs");
+      })
+      .catch(err => {
+        // ----> 'HERE WE CAPTURE THE ERROR'
+        console.log('There is a Failure', err)
+      })
 });
 
 
