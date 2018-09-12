e929ca0ac682d666ed6e363d6dcb3bffd58e073a
Paul Abarnou <p.abarnou37@gmail.com>
2018-09-12 18:51:34 +0200

    added css & fixed latest projects

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Project = require("../models/project-model.js");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index.hbs");
+  Project.find({}, {}, { sort: { 'created_at' : -1 } }, function(err, post) {})
+  .then(projectsArray => {
+    // res.send(projectsArray);
+    // res.locals.latestProjects = projectsArray;
+    res.locals.resultOne = projectsArray[0];
+    res.locals.resultTwo = projectsArray[1];
+    res.locals.resultThree = projectsArray[2];
+    res.render("index.hbs");
+  })
+  .catch(err => next(err));
+  
+  
+
 });
 //GET projects list page
 // router.get("/projects-list", (req, res, next) => {
